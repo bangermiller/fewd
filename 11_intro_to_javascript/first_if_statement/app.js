ad868929fb1ba9fa2a175f7c9a1b80cbf7d88203
bangermiller <bonnie@jwplayer.com>
2017-07-06 20:53:37 -0400

    Classwork

---
+++ 11_intro_to_javascript/first_if_statement/app.js
@@ -0,0 +1,8 @@
+
+	var number = parseInt (prompt ("Enter a number"));
+
+	if (number > 100) {
+		console.log("whoa, that's a really big number");
+	} else {
+		console.log ("lame, that's a small number");
+	}
---
+++ 11_intro_to_javascript/quiz/app.js
@@ -0,0 +1,8 @@
+
+	var name = prompt("What does GA stand for?");
+
+	if (name == "General Assembly") {
+		console.log("Correct!");
+	} else {
+		console.log ("CLose, but not quite. The answer is General Assembly.");
+	}
