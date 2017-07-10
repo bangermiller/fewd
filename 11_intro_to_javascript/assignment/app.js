a8d09e3668b65c0d226391403840eb2237c466c3
bangermiller <bonnie@jwplayer.com>
2017-07-10 14:58:32 -0400

    assignment11v2

--- 11_intro_to_javascript/assignment/app.js
+++ 11_intro_to_javascript/assignment/app.js
@@ -1,15 +1,19 @@
 
-var productPrice = parseInt ( prompt ("What is the price of the product you are buying?") );
-var inventoryItemNumber = parseInt ( prompt ("How many of the items of this product are in inventory?") );
-var money = parseInt ( prompt ("How much money do you have?") );
-var itemsToBuy = parseInt ( prompt ("How many items do you wish to buy?") );
+var productPrice = parseInt ( prompt ("What is the price of the product you are buying?"),10 );
+var inventoryCount = parseInt ( prompt ("How many of the items of this product are in inventory?"),10 );
+var money = parseInt ( prompt ("How much money do you have?"),10 );
+var itemsToBuy = parseInt ( prompt ("How many items do you wish to buy?"),10 );
 
-if(productPrice >= money && inventoryItemNumber > itemsToBuy) {
-	console.log("Purchase complete!");
-} else if(money < productPrice && inventoryItemNumber > itemsToBuy) {
-	console.log ("Not enough money");
-} else if  (productPrice >= money && inventoryItemNumber > itemsToBuy) {
-  console.log("Not enough inventory");
+
+if (inventoryCount > 0 && inventoryCount >= itemsToBuy) {
+	if (money >= productPrice) {
+		console.log ("purchase complete!");
+	} else if (money < productPrice) {
+		console.log ("Not enough money");
+	}	
+} else {
+		console.log ("Not enough inventory");
 }
 
-	
+
+
