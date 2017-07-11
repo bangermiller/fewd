d5f068f293cd22239212b0366a80318539e1eb2b
bangermiller <bonnie@jwplayer.com>
2017-07-10 21:08:59 -0400

    calculator

---
+++ 11_intro_to_javascript/assignment/calculator/calculator.js
@@ -0,0 +1,29 @@
+
+var numberOne = parseInt ( prompt ("Enter a number"),10 );
+var numberTwo = parseInt (prompt ("Enter a seconed number"),10);
+var operation = prompt ("Enter a calculation operation");
+
+
+if (operation == "+") {
+	console.log (numberOne + numberTwo);
+} else if (operation == "-") {
+	console.log (numberOne - numberTwo);
+} else if (operation == "*") {
+	console.log (numberOne * numberTwo);
+} else if (operation == "/") {
+	console.log (numberOne / numberTwo);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
